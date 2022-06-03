b6cbfd04a01563d77050309853f657a88a0ea103
getiso <getisog@gmail.com>
2022-06-03 05:29:38 -0700

    display the size of file types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,12 +11,12 @@ int main(void)
 	int b;
 	long int c;
 	long long int d;
-	float f;
+	float e;
 
 	printf("Size of char: %lu bytes\n", (unsigned long) sizeof(a));
 	printf("size of int: %lu bytes\n", (unsigned long)sizeof(b));
 	pritnf("Size of long int: %;u bytes\n", (unsigned long)sizeof(c));
 	printf("size of long long int: %lu bytes\n", (unsigned long)sizeof(d));
-	printf("size of float: %lu bytes\n", (unsigned long)sizeof(f));
+	printf("size of float: %lu bytes\n", (unsigned long)sizeof(e));
 	return (0);
 }

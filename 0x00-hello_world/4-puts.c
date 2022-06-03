b57372c73bcaa51555b1470bb6aa8ac194b6be3e
getiso <getisog@gmail.com>
2022-06-03 02:27:25 -0700

    printing string of text

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+/*
+ *
+ * main - enty point
+ *
+ *
+ * Return with 0 
+ *
+ */
+int main(viod)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

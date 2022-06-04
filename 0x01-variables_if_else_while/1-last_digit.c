c5c62cce992f69f666764bd9d24a7ccaa4efec10
getiso <getisog@gmail.com>
2022-06-04 05:38:58 -0700

    check the number greater, less or equal

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
- * main - entry point
+ * main - Entry point
  *
- * Return: always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
-	int l;
-	char str[] = "Last digit of";
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	l = n % 10;
 
-	if (l > 5)
-		pritnf("%s %d is %d and is greater than 5\n", str, n, l);
-	else if (l == 0)
-		printf("%s %d is %d and is 0\n", str, n, l);
-	else if (l < 6)
-		printf("%S %d is %d and is less than and not 0\n", str, n, l);
+	if (n % 10 > 5)
+		pritnf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+	else if (n % 10 == 0)
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	else 
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n n % 10);
+
 	return (0);
 }

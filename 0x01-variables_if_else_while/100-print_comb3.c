66732efae3aa5040fd328cd228f4094d5b37696a
getiso <getisog@gmail.com>
2022-06-04 08:29:32 -0700

    print all possible combination of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Print all possible combination of 2 digit
+ * Use only 'putchar' 5 times
+ * print in ascending order
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = 0;
+	int y;
+	int z;
+
+	while (x < 100)
+	{
+		y = i % 10; 
+		z = x / 10;
+
+		if (z < y)
+		{
+			putchar(z + '0');
+			putchar(y + '0');
+
+			if (x < 89)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+		x++;
+	}
+	putchar('\n');
+
+	return (0);
+}

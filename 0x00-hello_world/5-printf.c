10a10a89a008845b405a9f53e579f801d26f32e3
getiso <getisog@gmail.com>
2022-06-04 00:09:56 -0700

    print string of character

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - prints string character
- *
+ * main - entry point
+ * prints string character using printf
  * Return : Always 0 (success)
  */
-int main(void){
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }

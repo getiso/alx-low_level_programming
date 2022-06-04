1fc2b46540212310393c7ae03f1eef4b5bbcff29
getiso <getisog@gmail.com>
2022-06-04 07:25:39 -0700

    print all single digit number with only using putchar

--- 0x01-variables_if_else_while/6-print_nuberz.c
+++ 0x01-variables_if_else_while/6-print_nuberz.c
@@ -4,14 +4,14 @@
  * main - main block
  * Description: print all numbers of single digit
  * You are not allowed to use any variable char type
- * only use 'putchar' to print it
+ * only use 'putchar' to print all single digit number
  * Return: Always 0 (Succes)
  */
 int main(void)
 {
 	int j = 0;
 
-	while(i < 10)
+	while(j < 10)
 	{
 		putchar(j + '0');
 		j++;

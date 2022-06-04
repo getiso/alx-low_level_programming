d4b8ca85cfadb01354471040bbfd43f151e5bcf6
getiso <getisog@gmail.com>
2022-06-04 02:53:03 -0700

    print the quote message

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 /*
  * main - entry point
  * Retur: always 1 (Success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
-		
 }

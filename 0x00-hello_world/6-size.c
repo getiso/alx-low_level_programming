e0fc9eca5c4cbbc12f43587f8e771ef4ca5ba776
getiso <getisog@gmail.com>
2022-06-04 01:26:14 -0700

    different data types size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %ld bytes\n", (unsigned long)sizeof(char));
-	printf("size of an int: %ld bytes\n", (unsigned long)sizeof(int));
-	printf("Size of a long int: %ld bytes\n", (unsigned long)sizeof(long int));
-	printf("size of a long long int: %ld bytes\n", (unsigned long)sizeof(long long int));
-	printf("size of a float: %ld bytes\n", (unsigned long)sizeof(float));
+	printf("Size of a char: %ld byte(s)\n", (unsigned long)sizeof(char));
+	printf("size of an int: %ld byte(s)\n", (unsigned long)sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", (unsigned long)sizeof(long int));
+	printf("size of a long long int: %ld byte(s)\n", (unsigned long)sizeof(long long int));
+	printf("size of a float: %ld byte(s)\n", (unsigned long)sizeof(float));
 	return (0);
 }

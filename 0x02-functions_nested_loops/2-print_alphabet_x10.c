35761fb952103313ec6b0d79a2d65e8c4bc7d1b8
getiso <getisog@gmail.com>
2022-06-07 08:11:05 -0700

    print lower alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - print 10 times lowercase alphabet
+ * print_alphabet_x10 - print 10 times lowercase alphabets
  */
 void print_alphabet_x10(void)
 {
 	char chr;
 	int j = 1;
-
+	
 	while (j < 11)
 	{
 		chr = 'a';

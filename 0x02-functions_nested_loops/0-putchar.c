f76e25c3733bdb5eb32e8b295ee12fb860e99d9c
getiso <getisog@gmail.com>
2022-06-07 06:32:28 -0700

    write a program to _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main entry
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *chr = "_putchar";
+
+	while (*chr)
+	{
+		_putchar("%s", *chr);
+		chr++;
+	}
+	_putchar('\n');
+	
+	return (0);
+}

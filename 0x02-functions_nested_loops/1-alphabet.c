c21e85a5fc382aebb55a9d481fc295deecb7e1d7
getiso <getisog@gmail.com>
2022-06-07 07:31:41 -0700

    print lower alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print lower alphabet
+ */
+void print_alphabet(void)
+{
+	char chr = 'a';
+
+	while (chr <= 'z')
+	{
+		_putchar(chr);
+		chr++;
+	}
+	_putchar('\n')
+}

2faae09bb37a3fc9b6e071de46d73f9b2e205023
getiso <getisog@gmail.com>
2022-06-08 07:19:52 -0700

    nested loop task

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha: check the character
+ * @c: check c is character or not
+ * Return: return 1 if c is letter or return 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c <= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0)
+}

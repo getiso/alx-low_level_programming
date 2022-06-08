7a392ec5712d9dc308a9a56b1f7a2368349a2070
getiso <getisog@gmail.com>
2022-06-08 04:55:26 -0700

    nested loop task

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * islower - check is it lower or not
+ * Return: return 1 if c is lower return 0 otherwise
+ */
+int _islower(int c);
+{
+	if (c == 'az')
+		return (1);
+	else
+		return (0);
+}

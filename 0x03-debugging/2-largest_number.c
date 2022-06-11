9c71abfc9701e25829b6bf33af4f25b1a23e0a66
getiso <getisog@gmail.com>
2022-06-11 06:34:20 -0700

    0x03 C debugging

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+int largest_number(int a, int b, int c)
+{
+	int largest;
+	
+	if (a >= b && a > c) /*add = sign*/
+	{
+		largest = a;
+	}
+	else if (b >= a && a >= c) /*add = sign*/
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+	
+	return (largest);
+}

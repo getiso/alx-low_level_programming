32758c038f2b41c3d1279f5c272a79458b6a6171
getiso <getisog@gmail.com>
2022-06-18 16:07:12 +0300

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
- * _strncat - append src string to the dest string
+ * _strncat - concatenate n bytes src string to the dest string
  * @dest: string to append src
  * #src: string to append to dest
- * @n: largest number of bytes to append
- * Return: dest address
+ * @n: number of bytes to concatenate
+ *
+ * Return: address of des
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	int x, y;
 
-	x = y = 0;
+	x = 0;
+	y = 0;
 	while (*(dest + x))
 		x++;
 	while (y < n && *(src + y))

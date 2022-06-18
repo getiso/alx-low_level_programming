df0c5cab8e9db31d4c046d65c677426add477374
getiso <getisog@gmail.com>
2022-06-18 16:23:36 +0300

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,7 +3,7 @@
 /**
  * _strncat - concatenate n bytes src string to the dest string
  * @dest: string to append src
- * #src: string to append to dest
+ * @src: string to append to dest
  * @n: number of bytes to concatenate
  *
  * Return: address of des

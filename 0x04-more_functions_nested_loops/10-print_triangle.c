12e44b727d93558ce2988fd80eeb23b025d0d0c3
getiso <getisog@gmail.com>
2022-06-19 14:49:49 +0300

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,9 +1,25 @@
 #include "main.h"
 
 /**
- * print_triangle -
+ * print_triangle - prints a triangle of size size
  * @size: the size of triangle
+ * Return: void
  */
 void print_triangle(int size)
 {
-	
+	int a, b;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (a = 1; a <= size; a++)
+	{
+		for (b = 1; b <= size; b++)
+		{
+			if (b <= (size - a))
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+}

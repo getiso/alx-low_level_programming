f501c0ea452d728974372629a65915615c51a746
getiso <getisog@gmail.com>
2022-06-21 07:29:12 +0300

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,9 +2,10 @@
 
 /**
  * _memcpy - copies memory area
- * @dest: destination to copy
- * @src: source
- * @n: copies n bytes from memory area
+ * @dest: destination of copy
+ * @src: source to copy
+ * @n: number of bytes copy from memory area
+ * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string to check
+ * @needle: substring to find
+ * Return: pointer to the beginning of the located substring or null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int x = 0, y = 0;
+
+	while (haystack[x])
+	{
+		while (needle[y] && (haystack[x] == needle[0]))
+		{
+			if (haystack[x + y] == needle[y])
+				y++;
+			else
+				break;
+		}
+		if (needle[y])
+		{
+			x++;
+			y = 0;
+		}
+		else
+			return (haystack + x);
+	}
+	return (0);
+}

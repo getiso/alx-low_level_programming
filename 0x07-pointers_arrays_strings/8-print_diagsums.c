a1b09bcde663fefb00aa763c21590319f0311976
getiso <getisog@gmail.com>
2022-06-22 08:25:04 +0300

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_diagsums - prints the sum of the two diagonals of a square matrix of integers
+ * print_diagsums - prints sum of two diagonals of square matrix
  * @a: pointer to start of matrix
  * @size: width of matrix column
  * Return: void

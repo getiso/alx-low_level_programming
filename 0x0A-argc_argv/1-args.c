ddb24f35eb43839aac2218231ce32988e61245a3
getiso <getisog@gmail.com>
2022-06-25 14:40:46 +0300

    argc and argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments
+ * @argc: number of arguments passed
+ * @argv: argument vectory 
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc -1);
+	return (0);
+}

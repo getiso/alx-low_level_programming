97529d62c067d70008d5edea265f545d5d35bd97
getiso <getisog@gmail.com>
2022-06-25 14:32:12 +0300

    argc and argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - prints its name followed newline
+ * main - prints its name
  * @argc: number of arguments passed
  * @argc: arguments vector of pointer to string
  * Return: Always 0 (Success)
  */
-int main(int argc _attribute_((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);

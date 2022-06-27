c6e4fef66670823513002d91b10c381c1e728ad2
getiso <getisog@gmail.com>
2022-06-27 19:48:06 +0300

    C - malloc, free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,17 +19,21 @@ char *str_concat(char *s1, char *s2)
 		s1 = nul;
 	if (s2 == NULL)
 		s2 = nul;
+
 	x1 = 0, x2 = 0;
 	while (*(s1 + x1))
 		x1++;
 	while (*(s2 + x2))
 		x2++;
+	
 	s = malloc(sizeof(char) * (x1 + x2 + 1));
+	
 	if (s == 0)
 		return (0);
+	
 	for (x = 0; x < x1; x++)
 		*(s + x) = *(s1 + x);
 	for (x = 0, y = x1; x <= x2; y++, x++)
-		*(s + y) = *(x2 + x);
+		*(s + y) = *(s2 + x);
 	return (s);
 }

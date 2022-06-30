8cff157c447add118a2547d670fc2115cde2ce27
getiso <getisog@gmail.com>
2022-06-30 17:28:07 +0300

    more malloc and free

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * array_range - creates an array of integers
+ * @min: first integer
+ * @max: second integer
+ * Return: pointer to new allocation of memory
+ */
+int *array_range(int min, int max)
+{
+	int x, l;
+	int *e;
+
+	if (min > max)
+		return (NULL);
+	l = max - min + 1;
+	e = malloc(sizeof(int) * l);
+	if (e == NULL)
+		return (NULL);
+	for (x = 0; x < l; x++, min++)
+	{
+		e[x] = min;
+	}
+	return (e);
+}
